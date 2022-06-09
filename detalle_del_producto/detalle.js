d0cfa094a9f792e6cb3312b3fc9d3aedd363b06e
GerHw <german24hwang@gmail.com>
2022-06-08 21:12:22 -0300

    detalle del producto

---
+++ detalle_del_producto/detalle.js
@@ -0,0 +1,13 @@
+const express = require("express");
+const path = require("path");
+const app = express()
+
+app.use(express.static("public"));
+
+app.listen(3000, ()=> {
+    console.log("Servidor corriendo en puerto 3000")
+});
+
+app.get ("/", (req, res) => {
+    res.sendFile(path.join(__dirname, "./views/detalle-del-producto.html"))
+});
